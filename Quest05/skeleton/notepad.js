4d202a67386095c7b55b0c1d195a5fd67a23d6db
Dwn <kdw6158@gmail.com>
2021-09-17 18:02:43 +0900

    0917

--- Quest05/skeleton/notepad.js
+++ Quest05/skeleton/notepad.js
@@ -1,7 +1,27 @@
 class Notepad {
   /* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
-  constructor(name, msg) {
-    this.name = name;
+  constructor(title, msg) {
+    this.title = title;
     this.msg = msg;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  setTitle(newTitle) {
+    this.title = newTitle;
+  }
+
+  getMsg() {
+    return this.msg;
+  }
+
+  setMsg(newMsg) {
+    this.msg = newMsg;
+  }
+
+  newMsg(msg) {
+    localStorage.setItem(this.title, JSON.stringify(msg));
   }
 }
--- Quest07/skeleton/cjs-my-project/index.js
+++ Quest07/skeleton/cjs-my-project/index.js
@@ -1,1 +1,12 @@
 // TODO: cjs-package와 esm-package의 함수와 클래스들을 가져다 쓰고 활용하려면 어떻게 해야 할까요?
+const cjs = require('../cjs-package/index');
+
+// cjs.foo = 'foo';
+
+// console.log(cjs_out.double());
+
+console.log(cjs.CjsUtilClass);
+
+// const test = new cjs('test');
+
+// console.log(test.double());
